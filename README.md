016f5a267fe3b6fdeda19804bf647122c3eb8cf2
carlosp <carlosmpr0508@gmail.com>
2021-09-16 15:55:12 -0400

    PanHandler

---
+++ README.md
@@ -0,0 +1,8 @@
+# Basic React Native 
+A basic react native theme intro, for feuture project reference and reusable components
+
+## Featuring
+1. TypeScript
+2. PanHandler
+3. Screen Navigation
+4. Flatlist
---
+++ components/card/Card.tsx
@@ -0,0 +1,55 @@
+import React from "react";
+import { Text, View } from "../Themed";
+import { StyleSheet, Animated } from "react-native";
+import { PanGestureHandler } from "react-native-gesture-handler";
+
+export default function Card() {
+  let translateX = new Animated.Value(0);
+  let translateY = new Animated.Value(0);
+  let onPanGestureEvent = Animated.event(
+    [
+      {
+        nativeEvent: {
+          translationX: translateX,
+          translationY: translateY,
+        },
+      },
+    ],
+    { useNativeDriver: true }
+  );
+  return (
+    <PanGestureHandler onGestureEvent={onPanGestureEvent}>
+      <Animated.View
+        style={[
+          styles.card,
+          {
+            transform: [
+              {
+                translateX: translateX,
+              },
+              {
+                translateY: translateY,
+              },
+            ],
+          },
+        ]}
+      >
+       
+      </Animated.View>
+    </PanGestureHandler>
+  );
+}
+
+const styles = StyleSheet.create({
+  card: {
+    width: "50%",
+    height: "30%",
+    backgroundColor: "red",
+    padding: 25,
+    borderRadius: 25,
+    shadowOpacity: 0.75,
+    shadowRadius: 5,
+    shadowColor: "red",
+    shadowOffset: { height: 0, width: 0 },
+  },
+});
